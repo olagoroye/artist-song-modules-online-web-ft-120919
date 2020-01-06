765e577b68d04d4710cd0fa8f9813e3e8688c2d7
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-06 11:22:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,6 +9,7 @@ class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable::ClassMethods
+  include Paramable::InstanceMethods
 
   def initialize
     super
