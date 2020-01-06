9ff9e1424ce0f8c8e675b31ab63d4c237970ed65
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-06 10:56:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
@@ -33,7 +33,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-end
+#   def to_param
+#     name.downcase.gsub(' ', '-')
+#   end
+# end
